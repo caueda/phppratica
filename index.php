967a665f6659323378a6e93a868a89c446fdf90c
Carlos R. Ueda <carlos.ueda@gmail.com>
2016-04-02 19:10:16 -0400

    course register

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <html>
 <head>
+    <?php header('Content-Type: text/html; charset=iso-8859-1'); ?>
 	<title>Lista de Alunos</title>
 	<?php include("header.php")?>	
     <script>
     $(document).ready(function(){
 	    $('#menu').puimenubar();
     });
+    function mensagem(){
+        alert('Este aluno j√° est√° inscrito em um curso.');
+    }
     </script>
 </head>
 <body>
 	<div class="container">
-		<h2>P·gina Inicial</h2>		
+		<h2>P√°gina Inicial</h2>		
 		<ul  id="menu">
 			<li><a href="cadastro.php"><i class="icon-plus"></i>Incluir</a></li>
 			<li><a href="index.php"><i class="icon-refresh"></i>Refresh</a></li>		
@@ -18,15 +22,17 @@
 		<br>
 		<?php
 			include("bd.php");
-			$rs = $con->query("SELECT id_aluno, nome, matricula, idade, id_departamento FROM aluno");
+			$rs = $con->query("SELECT a.id_aluno, a.nome, a.matricula, a.idade, a.id_departamento, d.nome as nome_depto, c.nome as curso FROM aluno a LEFT JOIN departamento d ON a.id_departamento = d.id_departamento LEFT JOIN aluno_curso ac on ac.id_aluno = a.id_aluno LEFT JOIN curso c ON ac.id_curso = c.id_curso");
 			echo '<div class="table-responsive">';
 			echo '<table class="table table-bordered table-hover table-condensed">'; 
 			echo '<thead><tr>'.
 		           '<th>ID</th>'.
 		           '<th>Nome</th>'.
-		           '<th>MatrÌcula</th>'.
+		           '<th>Matr√≠cula</th>'.
 		           '<th>Idade</th>'.
-			       '<th>AÁ„o</th>'.
+                   '<th>Departamento</th>'.
+                   '<th>Curso</th>'.
+			       '<th>A√ß√£o</th>'.
 		         '</tr></thead>';
 			while($row = $rs->fetch(PDO::FETCH_OBJ)){
 				echo '<tr>'; 
@@ -34,10 +40,17 @@
 				echo '<td>' . $row->nome . '</td>'; 
 				echo '<td>' . $row->matricula . '</td>';
 				echo '<td>' . $row->idade . '</td>';
+                echo '<td>' . $row->nome_depto . '</td>';
+                echo '<td>' . $row->curso . '</td>';
 				echo '<td>';
-				echo '<a href="removido.php?id=' . $row->id_aluno . '"><i class="icon-trash"></i>&nbsp;Remover</a>&nbsp;/&nbsp;';
-				echo '<a href="atualizar.php?idaluno='.$row->id_aluno . '&nome='. $row->nome.'&matricula='.$row->matricula.'&idade='.$row->idade.'&departamento='.$row->id_departamento.'"><i class="icon-edit"></i>&nbsp;Atualizar</a>&nbsp;';
-				echo '</td>';
+				echo '<a href="removido.php?id=' . $row->id_aluno . '"><i class="icon-trash"></i>&nbsp;Remover</a>&nbsp;/&nbsp;';				
+                echo '<a href="atualizar.php?idaluno='.$row->id_aluno . '&nome='. $row->nome.'&matricula='.$row->matricula.'&idade='.$row->idade.'&departamento='.$row->id_departamento.'"><i class="icon-edit"></i>&nbsp;Atualizar&nbsp;/</a>&nbsp;';
+                if(is_null($row->curso)){
+                    echo '<a href="inscricao.php?idaluno='.$row->id_aluno . '&nome='. $row->nome.'&iddepartamento='.$row->id_departamento.'"><i class="icon-edit"></i>&nbsp;Inscrever</a>&nbsp;';
+				} else {
+                    echo '<a href="#" onclick="javascript:mensagem();"><i class="icon-edit"></i>&nbsp;Inscrever</a>';
+                }
+                echo '</td>';
 				echo '</tr>'; 
 			}
 			echo '</table>';	
---
+++ inscricao.php
@@ -0,0 +1,106 @@
+<html>
+<head>
+	<title>Inscrição</title>
+	<?php include("header.php")?>
+	<script>
+		function validar(){
+			return true;
+		}
+		$(document).ready(function(){
+			$("#atualizar").click(function(){
+				return validar();
+			});
+			$("#cancelar").click(function(){
+				window.location.href='index.php';
+			});				 
+		});		
+        $(function() {
+            $( "#datacadastro" ).datepicker();
+        });
+	</script>
+</head>
+<body>
+<?php 
+	include("bd.php");
+	try {		
+		if(isset($_POST["inscrever"])){
+			$idcurso=$_POST["idcurso"];
+			$idaluno=$_POST["idaluno"];
+			$datacadastro=$_POST["datacadastro"];
+			$iddepartamento=$_POST["iddepartamento"];
+            
+			$stmt = $con->prepare("INSERT INTO aluno_curso (id_aluno,id_curso,data_cadastro) VALUES(:idaluno, :idcurso, STR_TO_DATE(:datacadastro,'%d/%m/%Y'))");						
+			$success=$stmt->execute(array(
+			    ":idaluno"=>$idaluno,
+				":idcurso"=>$idcurso,
+				":datacadastro"=>$datacadastro
+			));
+			$con = null;
+			if($success){
+				header('Location: index.php');
+			} 
+		}
+	} catch(PDOException $e){
+		echo '<table border="1"/>';
+		echo '<tr>';		
+		echo '<th align="center"><b>Erro: ' . $e->getMessage(). '</b></th>';
+		echo '</tr>';
+		echo '</table>';
+		exit();
+	}
+?>
+	<div class="container">	
+		<h3>Atualizar Aluno</h3>
+		<form class="form-inline" action="inscricao.php" method="post">
+			<input type="hidden" name="idaluno" value='<?= $_GET["idaluno"] ?>'/>
+			
+			<div class="form-group">
+				<label for="nome" class="col-sm-2 control-label">Nome:</label>
+				<div class="col-sm-10">
+                    <input type="text" id="nome" name="nome" value='<?= $_GET["nome"] ?>' readonly="readonly">
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="matricula" class="col-sm-2 control-label">Data de cadastro:</label>
+				<div class="col-sm-10">
+					<input id="datacadastro" type="text" name="datacadastro">
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="departamento" class="col-sm-2 control-label">Curso:</label>
+				<div class="col-sm-10">
+					<select id="idcurso" name="idcurso" class="form-control">
+						<option value="-1">Selecione</option>
+						<?php 					
+                            include("bd.php");                            
+                            try {
+                                $rs = $con->query("SELECT id_curso, nome FROM curso WHERE id_departamento = '" . $_GET['iddepartamento']  .  "' ORDER BY nome");							
+                                while($row = $rs->fetch(PDO::FETCH_OBJ)){								 
+                                    echo '<option value="' . $row->id_curso . '">' . $row->nome .'</option>';															
+                                }		
+                            } catch(PDOException $e){
+                                echo '<table border="1"/>';
+                                echo '<tr>';		
+                                echo '<th align="center"><b>Erro: ' . $e->getMessage(). '</b></th>';
+                                echo '</tr>';
+                                echo '</table>';
+                                exit();
+                            }
+							$con = null;
+						?>
+					</select>
+				</div>
+			</div>			
+			<br>
+			<div class="form-group">
+				<div class="col-sm-offset-2 col-sm-10">
+			    	<button id="inscrever" name="inscrever" type="submit" class="btn btn-primary">Inscrever</button>
+			    	<button id="cancelar" name="cancelar" type="button" class="btn btn-primary">Cancelar</button>
+			   	</div>			   	
+			</div>	
+		</form>
+	</div>		
+	</form>
+
+</body>
+</html>
