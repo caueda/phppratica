0c08720f2709cc4ffa653b760c0ecfc8a907af80
Carlos R. Ueda <carlos.ueda@gmail.com>
2016-04-03 00:22:06 -0400

    including inactivate function

--- index.php
+++ index.php
@@ -22,7 +22,23 @@
 		<br>
 		<?php
 			include("bd.php");
-			$rs = $con->query("SELECT a.id_aluno, a.nome, a.matricula, a.idade, a.id_departamento, d.nome as nome_depto, c.nome as curso 
+			if(isset($_GET["inativar_inscricao"])){
+				$idcurso = $_GET["idcurso"];
+				$idaluno = $_GET["idaluno"];
+				
+				$stmt = $con->prepare("UPDATE aluno_curso SET ativo=0 WHERE id_curso =:idcurso and id_aluno = :idaluno");
+				$success=$stmt->execute(array(
+						":idcurso"=>$idcurso,
+						":idaluno"=>$idaluno
+				));
+				
+				if($success){
+					header('Location: index.php');
+				}
+				
+			}
+			
+			$rs = $con->query("SELECT a.id_aluno, a.nome, a.matricula, a.idade, a.id_departamento, d.nome as nome_depto, c.nome as curso, c.id_curso
 					FROM aluno a LEFT JOIN departamento d ON a.id_departamento = d.id_departamento 
 					LEFT JOIN aluno_curso ac on ac.id_aluno = a.id_aluno and ac.ativo = 1 
 					LEFT JOIN curso c ON ac.id_curso = c.id_curso
@@ -50,10 +66,11 @@
 				echo '<a href="removido.php?id=' . $row->id_aluno . '"><i class="icon-trash"></i>&nbsp;Remover</a>&nbsp;/&nbsp;';				
                 echo '<a href="atualizar.php?idaluno='.$row->id_aluno . '&nome='. $row->nome.'&matricula='.$row->matricula.'&idade='.$row->idade.'&departamento='.$row->id_departamento.'"><i class="icon-edit"></i>&nbsp;Atualizar&nbsp;</a>/&nbsp;';
                 if(is_null($row->curso)){
-                    echo '<a href="inscricao.php?idaluno='.$row->id_aluno . '&nome='. $row->nome.'&iddepartamento='.$row->id_departamento.'"><i class="icon-edit"></i>&nbsp;Inscrever</a>&nbsp;';
+                    echo '<a href="inscricao.php?idaluno='.$row->id_aluno . '&nome='. $row->nome.'&iddepartamento='.$row->id_departamento.'"><i class="icon-check"></i>&nbsp;Inscrever</a>&nbsp;/&nbsp;';
 				} else {
-                    echo '<a href="#" onclick="javascript:mensagem();"><i class="icon-edit"></i>&nbsp;Inscrever</a>';
+                    echo '<a href="#" onclick="javascript:mensagem();"><i class="icon-check"></i>&nbsp;Inscrever</a>&nbsp;/&nbsp;';
                 }
+                echo '<a href="index.php?inativar_inscricao=1&idcurso=' . $row->id_curso .'&idaluno='.$row->id_aluno.'"><i class="icon-remove-sign"></i>&nbsp;Cancelar Inscrição&nbsp;</a>';
                 echo '</td>';
 				echo '</tr>'; 
 			}
